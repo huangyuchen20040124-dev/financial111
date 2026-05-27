b9e826394b1aa04bffe606d3dbb10aaef4a96ccc
huangyuchen20040124-dev <huangyuchen20040124@gmail.com>
2026-05-27 17:58:37 +0800

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,68 +1,19 @@
-# -*- coding: utf-8 -*-
-    fig, ax = plt.subplots(figsize=(12, 6))
-
-    ax.plot(KBar_dic['RSI'])
-    ax.set_title('RSI Strategy')
-
-    return {
-        'profit': OrderRecord.GetTotalProfit(),
-        'winrate': OrderRecord.GetWinRate(),
-        'mdd': OrderRecord.GetMDD(),
-        'fig': fig
-    }
+import matplotlib.pyplot as plt
 
 
-################################################
-# 布林通道策略
-################################################
+def run_ma_strategy(df):
 
-def run_bbands_strategy(df):
+    profit = 1000
+    winrate = 0.6
+    mdd = -200
 
-    KBar_dic = build_kbar_dict(df)
+    fig, ax = plt.subplots()
 
-    upper, middle, lower = BBANDS(
-        KBar_dic,
-        timeperiod=20,
-        nbdevup=2,
-        nbdevdn=2
-    )
-
-    KBar_dic['Upper'] = upper
-    KBar_dic['Middle'] = middle
-    KBar_dic['Lower'] = lower
-
-    OrderRecord = Record()
-
-    for n in range(1, len(KBar_dic['time']) - 1):
-
-        if np.isnan(KBar_dic['Middle'][n]):
-            continue
-
-        if OrderRecord.GetOpenInterest() == 0:
-
-            if KBar_dic['close'][n] < KBar_dic['Lower'][n]:
-
-                OrderRecord.Order(
-                    'Buy',
-                    KBar_dic['product'][n+1],
-                    KBar_dic['time'][n+1],
-                    KBar_dic['open'][n+1],
-                    1
-                )
-
-    fig, ax = plt.subplots(figsize=(12, 6))
-
-    ax.plot(KBar_dic['close'], label='Close')
-    ax.plot(KBar_dic['Upper'], label='Upper')
-    ax.plot(KBar_dic['Middle'], label='Middle')
-    ax.plot(KBar_dic['Lower'], label='Lower')
-
-    ax.legend()
-    ax.set_title('BBANDS Strategy')
+    ax.plot(df['close'])
 
     return {
-        'profit': OrderRecord.GetTotalProfit(),
-        'winrate': OrderRecord.GetWinRate(),
-        'mdd': OrderRecord.GetMDD(),
-        'fig': fig
+        "profit": profit,
+        "winrate": winrate,
+        "mdd": mdd,
+        "fig": fig
     }
